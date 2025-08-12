6bc9dbb9ba77794293df789de1c27274ee353358
Alisherovna1 <ibodat2610@gmail.com>
2025-08-12 21:09:40 +0500

    Add files via upload

---
+++ lesson-1/homework/lesson 1.sql
@@ -0,0 +1,38 @@
+--Create class
+1) DB NAME READBOOKSDB
+TABLE NAME BOOKS
+COLUMNS:
+ BOOKID, int
+NAME, varchar
+AUTHOR, 
+GENRE, 
+STATUS char
+--Create database READBOOKSDB
+go
+use READBOOKSDB
+create table books (bookid int,name varchar (30),author varchar (30),
+,genry varchar (30),status varchar (30)
+select * from books 
+
+insert into books values (1,'Shinnelli yillar'
+
+
+
+select * from [HumanResources].[Employee]
+where Gender='M'
+
+CREATE TABLE classes (
+    class_id INT,
+    class_name VARCHAR(50),
+    teacher_name VARCHAR(50)
+);
+
+INSERT INTO classes (class_id, class_name, teacher_name) VALUES
+(1, 'Math 101', 'Mr. Smith'),
+(2, 'English 102', 'Ms. Johnson'),
+(3, 'Science 103', 'Dr. Lee');
+
+select * from
+
+update books
+set name='Fooled by randomness'
